1beaeb570b184c34bec2454fb884a5735a681f06
coderaiser <mnemonic.enemy@gmail.com>
2014-07-24 04:27:06 -0400

    refactor(server) mv start to console

--- bin/console.js
+++ bin/console.js
@@ -3,11 +3,25 @@
 (function() {
     'use strict';
     
-    var server = require('../lib/server');
-    
-    server.start({
-        port: 9999,
-        ip: '0.0.0.0'
-    });
-    
+    var webconsole  = require('../'),
+        http        = require('http'),
+        express     = require('express'),
+        
+        app         = express(),
+        server      = http.createServer(app),
+        
+        port        =   process.env.PORT            ||  /* c9           */
+                        process.env.app_port        ||  /* nodester     */
+                        process.env.VCAP_APP_PORT   ||  /* cloudfoundry */
+                        1337,
+        
+        ip          =   process.env.IP              ||  /* c9           */
+                        '0.0.0.0';
+        
+        webconsole(server);
+        
+        app.use(express.static(__dirname + '/../'));
+        
+        server.listen(port, ip);
+        console.log('url: http://' + ip + ':' + port);
 })();
