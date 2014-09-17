ab4d46f3baa6118fcd7ebb5d9382e9a35741f70b
coderaiser <mnemonic.enemy@gmail.com>
2014-09-17 07:15:51 -0400

    feature(console) add -v, --v

--- bin/console.js
+++ bin/console.js
@@ -3,7 +3,24 @@
 (function() {
     'use strict';
     
-    var DIR         = __dirname + '/../',
+    var argv        = process.argv,
+        argvLast    = argv.slice().pop();
+    
+    switch (argvLast) {
+    case '-v':
+        version();
+        break;
+    
+    case '--v':
+        version();
+        break;
+    
+    default:
+        start();
+    }
+    
+    function start() {
+        var DIR         = __dirname + '/../',
         
         webconsole  = require('../'),
         http        = require('http'),
@@ -32,5 +49,13 @@
             .use(express.static(DIR));
         
         server.listen(port, ip);
+        
         console.log('url: http://' + ip + ':' + port);
+    }
+    
+    function version() {
+        var pack = require('../package.json');
+        
+        console.log('v' + pack.version);
+    }
 })();
