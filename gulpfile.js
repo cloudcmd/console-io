b01acc1d792cfa3a83a1db208f26026d51b87721
coderaiser <coderaiser@cloudcmd.io>
2016-10-26 10:42:13 +0300

    feature(gulpfile) rm iifee

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,39 @@
-(function() {
-    'use strict';
+'use strict';
     
-    var gulp        = require('gulp'),
-        jshint      = require('gulp-jshint'),
-        recess      = require('gulp-recess'),
-        
-        LIB         = 'lib/',
-        LIB_CLIENT  = LIB + 'client/',
-        LIB_SERVER  = LIB + 'server/',
-        Src         = [
-            '*.js',
-            'gulp/**/*.js',
-            LIB + '*.js',
-            LIB_CLIENT + '*.js',
-            LIB_SERVER + '**/*.js',
-            '!' + LIB_CLIENT + 'jquery.js'
-        ];
+const gulp        = require('gulp'),
+    jshint      = require('gulp-jshint'),
+    recess      = require('gulp-recess'),
     
-    gulp.task('jshint', function() {
-        gulp.src(Src)
-            .pipe(jshint())
-            .pipe(jshint.reporter())
-            .on('error', onError);
-    });
-    
-   
-    gulp.task('css', function () {
-        gulp.src('css/*.css')
-            .pipe(recess())
-            .pipe(recess.reporter())
-            .on('error', onError);
-    });
-    
-    gulp.task('default', ['jshint', 'css']);
-    
-    function onError(params) {
-        console.log(params.message);
-    }
-    
-})();
+    LIB         = 'lib/',
+    LIB_CLIENT  = LIB + 'client/',
+    LIB_SERVER  = LIB + 'server/',
+    Src         = [
+        '*.js',
+        'gulp/**/*.js',
+        LIB + '*.js',
+        LIB_CLIENT + '*.js',
+        LIB_SERVER + '**/*.js',
+        '!' + LIB_CLIENT + 'jquery.js'
+    ];
+
+gulp.task('jshint', () => {
+    gulp.src(Src)
+        .pipe(jshint())
+        .pipe(jshint.reporter())
+        .on('error', onError);
+});
+
+
+gulp.task('css', () => {
+    gulp.src('css/*.css')
+        .pipe(recess())
+        .pipe(recess.reporter())
+        .on('error', onError);
+});
+
+gulp.task('default', ['jshint', 'css']);
+
+function onError(params) {
+    console.log(params.message);
+}
+
