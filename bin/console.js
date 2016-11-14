d2cf17b76afcaf5764902eb87b6ccbc447e527fc
coderaiser <coderaiser@cloudcmd.io>
2016-11-14 17:38:50 +0200

    chore(console) rm iife

--- bin/console.js
+++ bin/console.js
@@ -1,62 +1,60 @@
 #!/usr/bin/env node
 
-(function() {
-    'use strict';
+'use strict';
+
+var argv        = process.argv,
+    argvLast    = argv.slice().pop();
+
+switch (argvLast) {
+case '-v':
+    version();
+    break;
+
+case '--v':
+    version();
+    break;
+
+default:
+    start();
+}
+
+function start() {
+    var DIR         = __dirname + '/../',
     
-    var argv        = process.argv,
-        argvLast    = argv.slice().pop();
+    webconsole  = require('../'),
+    http        = require('http'),
     
-    switch (argvLast) {
-    case '-v':
-        version();
-        break;
+    express     = require('express'),
+    mollify     = require('mollify'),
     
-    case '--v':
-        version();
-        break;
+    app         = express(),
+    server      = http.createServer(app),
     
-    default:
-        start();
-    }
+    port        =   process.env.PORT            ||  /* c9           */
+                    process.env.app_port        ||  /* nodester     */
+                    process.env.VCAP_APP_PORT   ||  /* cloudfoundry */
+                    1337,
     
-    function start() {
-        var DIR         = __dirname + '/../',
-        
-        webconsole  = require('../'),
-        http        = require('http'),
-        
-        express     = require('express'),
-        mollify     = require('mollify'),
-        
-        app         = express(),
-        server      = http.createServer(app),
-        
-        port        =   process.env.PORT            ||  /* c9           */
-                        process.env.app_port        ||  /* nodester     */
-                        process.env.VCAP_APP_PORT   ||  /* cloudfoundry */
-                        1337,
-        
-        ip          =   process.env.IP              ||  /* c9           */
-                        '0.0.0.0';
-        
-        app .use(webconsole({
-                server: server,
-                online: false,
-                minify: false
-            }))
-            .use(mollify({
-                dir: DIR
-            }))
-            .use(express.static(DIR));
-        
-        server.listen(port, ip);
-        
-        console.log('url: http://' + ip + ':' + port);
-    }
+    ip          =   process.env.IP              ||  /* c9           */
+                    '0.0.0.0';
     
-    function version() {
-        var pack = require('../package.json');
-        
-        console.log('v' + pack.version);
-    }
-})();
+    app .use(webconsole({
+            server: server,
+            online: false,
+            minify: false
+        }))
+        .use(mollify({
+            dir: DIR
+        }))
+        .use(express.static(DIR));
+    
+    server.listen(port, ip);
+    
+    console.log('url: http://' + ip + ':' + port);
+}
+
+function version() {
+    var pack = require('../package.json');
+    
+    console.log('v' + pack.version);
+}
